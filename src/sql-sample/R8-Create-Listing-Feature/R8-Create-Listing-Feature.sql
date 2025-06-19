35cb5822b1d2a84118c394d0f7fcd73d11e18d6e
Franklin Ramirez <121467771+franklin611@users.noreply.github.com>
2025-06-19 14:03:30 -0400

    Adding R8 query (#7)

---
+++ src/sql-sample/R8-Create-Listing-Feature/R8-Create-Listing-Feature.sql
@@ -0,0 +1,26 @@
+INSERT INTO Listing (
+  seller_id,
+  type,
+  price,
+  title,
+  description,
+  product_condition,
+  quantity,
+  location,
+  posted_by,
+  status,
+  image_storage_ref
+)
+VALUES (
+  'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1',
+  'School Supplies',
+  10,
+  'Computer Bag',
+  'A pristine computer bag.',
+  'like new',
+  1,
+  'MC',
+  'Alice',
+  'for sale',
+  'images/bag.jpg' 
+);
