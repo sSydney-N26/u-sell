1fcdf5d4a5a08f42eb07cc0d325aad58098efb6d
helenaxu924 <helenaxu924@gmail.com>
2025-07-05 01:20:40 -0400

    edited setup

--- src/app/db/setup-db.sh
+++ src/app/db/setup-db.sh
@@ -14,14 +14,14 @@ mysql -u root -p -e "
 
 echo "Checking to see if u_sell table already exist, if so drop"
 mysql -u admin -p"admin1!" -e "
-    DROP DATABASE IF EXISTS u_sell; 
+    DROP DATABASE IF EXISTS u_sell;
     CREATE DATABASE u_sell;
     "
 echo "Creating schema..."
 mysql -u admin -p"admin1!" u_sell < create-schema.sql
 
-# Run the mock-data.sql script
-echo "Loading mock data..."
-mysql -u admin -p"admin1!" u_sell < mock-data.sql
+# Run the production data script
+echo "Loading production data..."
+mysql -u admin -p"admin1!" u_sell < usell_prod_data.sql
 
 echo "Database setup complete!"
