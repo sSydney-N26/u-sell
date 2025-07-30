dad75b4e838d3d369394aa714c7137c28521566f
Sydney Nguyen <sydney2004nguyen@gmail.com>
2025-07-29 23:30:37 -0400

    Update production data for FYP User Following

--- src/app/db/usell_prod_data.sql
+++ src/app/db/usell_prod_data.sql
@@ -3202,35 +3202,6 @@ INSERT INTO ListingTags (listing_id, tag_id) VALUES (1610, (SELECT tag_id FROM T
 INSERT INTO ListingTags (listing_id, tag_id) VALUES (1613, (SELECT tag_id FROM Tags WHERE tag_name = 'cup'));
 INSERT INTO ListingTags (listing_id, tag_id) VALUES (1613, (SELECT tag_id FROM Tags WHERE tag_name = 'drink'));
 
--- Popular users with many followers
--- uid x8uocqJbNoWO7TL6ZCEXCR2Hm1k1 is Alice, has 10 followers
-INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('40d6af98-9170-40ec-95b1-9f521e4958f6', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- barbara10 follows alice
-INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('83af10c1-2021-4eca-a9e5-76e6f4e9523b', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- jeffrey10 follows alice
-INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('5b5a7e61-14a4-4de1-9c3f-c093edeff64f', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- joshuajones follows alice
-INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('bdb6f3dd-a498-41ba-8728-096dff3bd330', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- blakeerik follows alice
-INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('2ab3410d-aa84-404e-a9f5-52caa19fe15b', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- curtis61 follows alice
-INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('325c8801-72e0-4f9f-aa7b-4de2c86ab3d4', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- yherrera follows alice
-
--- uid x8uocqJbNoWO7TL6ZCEXCR2Hm1k1 is Alice, follows 8
-INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('x8uocqJbNoWO7TL6ZCEXCR2Hm1k1', '2ab3410d-aa84-404e-a9f5-52caa19fe15b'); -- alice follows curtis61 (and vice versa)
-INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('x8uocqJbNoWO7TL6ZCEXCR2Hm1k1','325c8801-72e0-4f9f-aa7b-4de2c86ab3d4'); -- alice follows yherrera (and vice versa)
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("x8uocqJbNoWO7TL6ZCEXCR2Hm1k1", "3f9a1261-df93-4e67-9a77-09be06c35924");
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("x8uocqJbNoWO7TL6ZCEXCR2Hm1k1", "5af9ae2f-b972-4a19-b095-ed3a1be078c3");
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("x8uocqJbNoWO7TL6ZCEXCR2Hm1k1", "917e2c44-2103-4d80-b093-36e6270bac3b");
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("x8uocqJbNoWO7TL6ZCEXCR2Hm1k1", "bb076683-88d2-449f-9100-32ef4b0ba023");
-
--- Insert for Friends of Friends Suggestions (should get 6 suggestions in total)
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("5af9ae2f-b972-4a19-b095-ed3a1be078c3", "7ff59612-9d23-4501-824f-685d8c63967e");
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("5af9ae2f-b972-4a19-b095-ed3a1be078c3", "ddc899fd-5c20-4656-8c03-ec28bed09806");
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("917e2c44-2103-4d80-b093-36e6270bac3b", "ddc899fd-5c20-4656-8c03-ec28bed09806");
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("917e2c44-2103-4d80-b093-36e6270bac3b", "dc5dbe09-3d8e-486b-9254-902ffb1f0d86");
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("917e2c44-2103-4d80-b093-36e6270bac3b", "e2c03bd2-0312-4c5d-a080-c00675893c0d");
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("bb076683-88d2-449f-9100-32ef4b0ba023", "abf7b346-09c2-4f9b-8b58-3d5afd4ec73e");
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("bb076683-88d2-449f-9100-32ef4b0ba023", "cc2f1171-02be-4fb3-9ecc-5a78cdf78ff0");
-
--- This shouldn't get suggested because it is a friend of a user that Alice doesn't follow
-INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("7ff59612-9d23-4501-824f-685d8c63967e", "abf7b346-09c2-4f9b-8b58-3d5afd4ec73e");
-
 -- FYP (For You Page) Preference Data
 -- Adding realistic user preferences for categories, keywords, and tags
 
@@ -3779,6 +3750,71 @@ INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('6016e394-db8d-4bd3
 INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('98eb6e25-dbf6-481e-8e96-360d2bf259f7', 'b1321344-bbe9-4bf7-82b9-e1898f2e2d7d');
 INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('15b960cf-c9ee-4159-8fee-6c355f988172', '98eb6e25-dbf6-481e-8e96-360d2bf259f7');
 INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('ee5d296c-9dce-4588-b318-4eefd1433c01', '15b960cf-c9ee-4159-8fee-6c355f988172');
+
+-- Popular users with many followers
+-- uid x8uocqJbNoWO7TL6ZCEXCR2Hm1k1 is Alice, has 10 followers
+INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('40d6af98-9170-40ec-95b1-9f521e4958f6', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- barbara10 follows alice
+INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('83af10c1-2021-4eca-a9e5-76e6f4e9523b', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- jeffrey10 follows alice
+INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('5b5a7e61-14a4-4de1-9c3f-c093edeff64f', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- joshuajones follows alice
+INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('bdb6f3dd-a498-41ba-8728-096dff3bd330', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- blakeerik follows alice
+INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('2ab3410d-aa84-404e-a9f5-52caa19fe15b', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- curtis61 follows alice
+INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('325c8801-72e0-4f9f-aa7b-4de2c86ab3d4', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'); -- yherrera follows alice
+
+-- uid x8uocqJbNoWO7TL6ZCEXCR2Hm1k1 is Alice, follows 8
+INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('x8uocqJbNoWO7TL6ZCEXCR2Hm1k1', '2ab3410d-aa84-404e-a9f5-52caa19fe15b'); -- alice follows curtis61 (and vice versa)
+INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES ('x8uocqJbNoWO7TL6ZCEXCR2Hm1k1','325c8801-72e0-4f9f-aa7b-4de2c86ab3d4'); -- alice follows yherrera (and vice versa)
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("x8uocqJbNoWO7TL6ZCEXCR2Hm1k1", "3f9a1261-df93-4e67-9a77-09be06c35924");
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("x8uocqJbNoWO7TL6ZCEXCR2Hm1k1", "5af9ae2f-b972-4a19-b095-ed3a1be078c3");
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("x8uocqJbNoWO7TL6ZCEXCR2Hm1k1", "917e2c44-2103-4d80-b093-36e6270bac3b");
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("x8uocqJbNoWO7TL6ZCEXCR2Hm1k1", "bb076683-88d2-449f-9100-32ef4b0ba023");
+
+-- Insert for Friends of Friends Suggestions (should get 6 suggestions in total)
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("5af9ae2f-b972-4a19-b095-ed3a1be078c3", "7ff59612-9d23-4501-824f-685d8c63967e");
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("5af9ae2f-b972-4a19-b095-ed3a1be078c3", "ddc899fd-5c20-4656-8c03-ec28bed09806");
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("917e2c44-2103-4d80-b093-36e6270bac3b", "ddc899fd-5c20-4656-8c03-ec28bed09806");
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("917e2c44-2103-4d80-b093-36e6270bac3b", "dc5dbe09-3d8e-486b-9254-902ffb1f0d86");
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("917e2c44-2103-4d80-b093-36e6270bac3b", "e2c03bd2-0312-4c5d-a080-c00675893c0d");
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("bb076683-88d2-449f-9100-32ef4b0ba023", "abf7b346-09c2-4f9b-8b58-3d5afd4ec73e");
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("bb076683-88d2-449f-9100-32ef4b0ba023", "cc2f1171-02be-4fb3-9ecc-5a78cdf78ff0");
+
+-- This shouldn't get suggested because it is a friend of a user that Alice doesn't follow
+INSERT INTO UserFollowedUsers(user_id, followee_id) VALUES ("7ff59612-9d23-4501-824f-685d8c63967e", "abf7b346-09c2-4f9b-8b58-3d5afd4ec73e");
+INSERT INTO UserFollowedUsers (user_id, followee_id) VALUES
+('x8uocqJbNoWO7TL6ZCEXCR2Hm1k1', '40d6af98-9170-40ec-95b1-9f521e4958f6'),
+('40d6af98-9170-40ec-95b1-9f521e4958f6', 'c7a127cb-47a1-42a1-97f5-9df43a1afc9f'),
+('c7a127cb-47a1-42a1-97f5-9df43a1afc9f', 'x8uocqJbNoWO7TL6ZCEXCR2Hm1k1'),
+('bdb6f3dd-a498-41ba-8728-096dff3bd330', '131eb10a-55b6-4de5-8da4-0d860dc21ad8'),
+('badea862-e2a3-4315-b758-0f2d72b4afed', 'b1321344-bbe9-4bf7-82b9-e1898f2e2d7d'),
+('4a77c430-6444-4592-8118-87e71cf5a08f', 'c83233fa-97ac-4822-929e-d299d426cadf'),
+('c83233fa-97ac-4822-929e-d299d426cadf', 'bfa6b7ee-950d-4d2a-a875-d34b0a28becd'),
+('bfa6b7ee-950d-4d2a-a875-d34b0a28becd', '4a77c430-6444-4592-8118-87e71cf5a08f'),
+('6016e394-db8d-4bd3-8615-fbb0131db924', '2ab3410d-aa84-404e-a9f5-52caa19fe15b'),
+('b1863163-0cb4-49f3-abbc-053c304480b3', 'e90b6823-ce68-460b-8066-8c1e3022ed7d'),
+('325c8801-72e0-4f9f-aa7b-4de2c86ab3d4', 'ef758cc1-248b-487f-bb44-651b4cdc3397'),
+('ef758cc1-248b-487f-bb44-651b4cdc3397', '325c8801-72e0-4f9f-aa7b-4de2c86ab3d4'),
+('98eb6e25-dbf6-481e-8e96-360d2bf259f7', 'e7bf7248-dfd6-4222-a150-5e80a1ac2472'),
+('b8c898cf-b0d0-443e-8577-080e7d3f9c37', 'cb6a8295-e469-4930-8c6b-434b78945b79'),
+('x8uocqJbNoWO7TL6ZCEXCR2Hm1k1', '98eb6e25-dbf6-481e-8e96-360d2bf259f7'),
+('bdb6f3dd-a498-41ba-8728-096dff3bd330', 'e7bf7248-dfd6-4222-a150-5e80a1ac2472'),
+('8828dd3f-7f34-4181-af0a-6ca44514a97d', '1584f407-b518-40a0-a661-7fc396dfffbb'),
+('1584f407-b518-40a0-a661-7fc396dfffbb', '8828dd3f-7f34-4181-af0a-6ca44514a97d'),
+('bdb6f3dd-a498-41ba-8728-096dff3bd330', '40d6af98-9170-40ec-95b1-9f521e4958f6'),
+('c7a127cb-47a1-42a1-97f5-9df43a1afc9f', 'b1321344-bbe9-4bf7-82b9-e1898f2e2d7d'),
+('e90b6823-ce68-460b-8066-8c1e3022ed7d', 'b1863163-0cb4-49f3-abbc-053c304480b3'),
+('x8uocqJbNoWO7TL6ZCEXCR2Hm1k1', '4a77c430-6444-4592-8118-87e71cf5a08f'),
+('b1321344-bbe9-4bf7-82b9-e1898f2e2d7d', '6016e394-db8d-4bd3-8615-fbb0131db924'),
+('4a77c430-6444-4592-8118-87e71cf5a08f', '2ab3410d-aa84-404e-a9f5-52caa19fe15b'),
+('40d6af98-9170-40ec-95b1-9f521e4958f6', '8828dd3f-7f34-4181-af0a-6ca44514a97d'),
+('78a25057-dcbb-44f9-b990-61d5ab966ba2', '2ab3410d-aa84-404e-a9f5-52caa19fe15b'),
+('x8uocqJbNoWO7TL6ZCEXCR2Hm1k1', 'b8c898cf-b0d0-443e-8577-080e7d3f9c37'),
+('bfa6b7ee-950d-4d2a-a875-d34b0a28becd', 'cb6a8295-e469-4930-8c6b-434b78945b79'),
+('131eb10a-55b6-4de5-8da4-0d860dc21ad8', 'badea862-e2a3-4315-b758-0f2d72b4afed'),
+('x8uocqJbNoWO7TL6ZCEXCR2Hm1k1', '1584f407-b518-40a0-a661-7fc396dfffbb'),
+('c7a127cb-47a1-42a1-97f5-9df43a1afc9f', '8828dd3f-7f34-4181-af0a-6ca44514a97d'),
+('ee5d296c-9dce-4588-b318-4eefd1433c01', 'b1863163-0cb4-49f3-abbc-053c304480b3'),
+('x8uocqJbNoWO7TL6ZCEXCR2Hm1k1', '78a25057-dcbb-44f9-b990-61d5ab966ba2'),
+('bfa6b7ee-950d-4d2a-a875-d34b0a28becd', '38232638-b1db-4648-bf67-4603948bab98'),
+('c83233fa-97ac-4822-929e-d299d426cadf', '2ab3410d-aa84-404e-a9f5-52caa19fe15b');
 
 -- ===== COMPREHENSIVE TESTING DATA FOR REPORT LISTING FEATURE =====
 
